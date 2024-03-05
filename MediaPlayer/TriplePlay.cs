2b60b9e5694e1da49a6f1da5885fee360007a7db
Anthony C <47967077+ant-car@users.noreply.github.com>
2024-03-05 23:25:39 +1100

    Add TriplePlay

---
+++ MediaPlayer/TriplePlay.cs
@@ -0,0 +1,93 @@
+﻿using AVCoders.Core;
+
+namespace AVCoders.MediaPlayer;
+
+public class TriplePlay : MediaPlayer
+{
+    private readonly int _deviceId;
+    private readonly string _host;
+        
+    public TriplePlay(int deviceId, string host)
+    {
+        _deviceId = deviceId;
+        _host = host;
+    }
+
+    private async Task Get(string uri)
+    {
+        // Use HttpClient - HttpWebRequest seems to break after three requests.
+        using (HttpClient httpClient = new HttpClient())
+        {
+            try
+            {
+                Log($"Sending request: {uri}");
+                await httpClient.GetAsync(uri);
+            }
+            catch (Exception e)
+            {
+                Log(e.Message);
+            }
+        }
+    }
+
+    private string GenerateCommandString(string command)
+    {
+        return
+            $"http://{_host}/triplecare/JsonRpcHandler.php?call={{\"jsonrpc\":\"2.0\",\"method\":\"{command}\",\"params\":[{_deviceId}]}}";
+    }
+
+    private string GenerateCommandStringWithArgument(string command, string param)
+    {
+        return
+            $"http://{_host}/triplecare/JsonRpcHandler.php?call={{\"jsonrpc\":\"2.0\",\"method\":\"{command}\",\"params\":[{_deviceId},{param}]}}";
+    }
+
+    public void ChannelUp()
+    {
+        Get(GenerateCommandString("ChannelUp"));
+    }
+
+    public void ChannelDown()
+    {
+        Get(GenerateCommandString("ChannelDown"));
+    }
+
+    public void GoToServiceId(uint serviceId)
+    {
+        Get(GenerateCommandStringWithArgument("ChangeChannel", serviceId.ToString()));
+    }
+
+    public void GoToChannelNumber(uint channelNumber)
+    {
+        Get(GenerateCommandStringWithArgument("SelectChannel",channelNumber.ToString()));
+    }
+
+    public override void PowerOn()
+    {
+        Get(GenerateCommandString("PowerOnTv"));
+        PowerState = PowerState.On;
+    }
+
+    public override void PowerOff()
+    {
+        Get(GenerateCommandString("PowerOffTv"));
+        PowerState = PowerState.Off;
+    }
+
+    public void Reboot()
+    {
+        Get(GenerateCommandString("Reboot"));
+    }
+
+    public void SetVolume(int volume)
+    {
+        Get(GenerateCommandStringWithArgument("SetVolume",volume.ToString()));
+        Volume = volume;
+    }
+
+    public void SetAudioMute(MuteState state)
+    {
+        Get(GenerateCommandStringWithArgument("SetMute",state == MuteState.On? "true" : "false"));
+        AudioMute = state;
+    }
+}
