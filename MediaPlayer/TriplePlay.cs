0033d81c7adfda4f8cc853f4fff25faeb1dc4d1f
Anthony C <47967077+ant-car@users.noreply.github.com>
2024-03-07 20:04:29 +1100

    I want execution to continue

--- MediaPlayer/TriplePlay.cs
+++ MediaPlayer/TriplePlay.cs
@@ -1,4 +1,5 @@
 ﻿using AVCoders.Core;
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 
 namespace AVCoders.MediaPlayer;
 
