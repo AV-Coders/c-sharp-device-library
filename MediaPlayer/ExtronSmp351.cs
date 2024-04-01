b7c178e4df565e1d6abe275d514a6594e17ddd75
Anthony C <47967077+ant-car@users.noreply.github.com>
2024-04-01 23:00:41 +1100

    Start SMP-351 Module

---
+++ MediaPlayer/ExtronSmp351.cs
@@ -0,0 +1,79 @@
+﻿using System.Text.RegularExpressions;
+using AVCoders.Core;
+
+namespace AVCoders.MediaPlayer;
+
+public enum RecordState
+{
+    Unknown,
+    Recording,
+    RecordingPaused,
+    Stopped
+}
+
+public delegate void RecordStateHandler(RecordState state);
+
+public delegate void TimestampHandler(string timestamp);
+
+public class ExtronSmp351
+{
+    private readonly CommunicationClient _communicationClient;
+    public static readonly ushort DefaultPort = 22023;
+    private readonly ThreadWorker _pollWorker;
+    public RecordStateHandler? RecordStateHandlers;
+    public TimestampHandler? TimestampHandlers;
+    private RecordState _recordState;
+    private readonly Regex _responseParser;
+    private readonly ulong _memoryLowKBytes;
+    private readonly ulong _memoryFullKBytes;
+
+    public ExtronSmp351(CommunicationClient communicationClient, ulong memoryLowKBytes, ulong memoryFullKBytes, int pollTime = 1000)
+    {
+        _communicationClient = communicationClient;
+        _memoryLowKBytes = memoryLowKBytes;
+        _memoryFullKBytes = memoryFullKBytes;
+        _communicationClient.ResponseHandlers += HandleResponse;
+        _recordState = RecordState.Unknown;
+        _pollWorker = new ThreadWorker(PollRecorderThreadFunction, TimeSpan.FromMilliseconds(pollTime));
+        _pollWorker.Restart();
+
+        string responsePattern = "<([^>]*)>";
+        _responseParser = new Regex(responsePattern, RegexOptions.None, TimeSpan.FromMilliseconds(100));
+    }
+
+    private void HandleResponse(string response)
+    {
+        RecordStateHandlers?.Invoke(RecordState.Stopped);
+        var matches = _responseParser.Matches(response);
+        ProcessRecordingState(matches[1].Groups[1].Value);
+        if (_recordState is RecordState.Recording or RecordState.RecordingPaused)
+        {
+            TimestampHandlers?.Invoke(matches[4].Groups[1].Value);
+        }
+    }
+
+    private void ProcessRecordingState(string state)
+    {
+        RecordState currentState = state switch
+        {
+            "recording" => RecordState.Recording,
+            "paused" => RecordState.RecordingPaused,
+            "stopped" => RecordState.Stopped,
+            _ => RecordState.Unknown
+        };
+
+        if (currentState == _recordState)
+            return;
+        
+        RecordStateHandlers?.Invoke(currentState);
+        _recordState = currentState;
+    }
+
+    private void PollRecorderThreadFunction()
+    {
+        if(_communicationClient.GetConnectionState() == ConnectionState.Connected)
+        {
+            _communicationClient.Send("I");
+        }
+    }
+}
---
+++ MediaPlayerTest/ExtronSmp351Test.cs
@@ -0,0 +1,62 @@
+﻿using AVCoders.Core;
+
+namespace AVCoders.MediaPlayer.Tests;
+
+public class ExtronSmp351Test
+{
+    public abstract class StubbedClient : IpComms
+    {
+        protected StubbedClient(string host, ushort port) : base(host, port){}
+
+        public override void Send(string message){}
+
+        public override void Send(byte[] bytes){}
+
+        public override void SetPort(ushort port){}
+
+        public override void SetHost(string host){}
+        public new ConnectionState GetConnectionState() => ConnectionState.Connected;
+    }
+    
+    private ExtronSmp351 _recorder;
+    private readonly Mock<StubbedClient> _mockClient = new("foo", (ushort)1);
+    private readonly Mock<RecordStateHandler> _recordStateHandler = new();
+    private readonly Mock<TimestampHandler> _timestampHandler = new ();
+
+    public ExtronSmp351Test()
+    {
+        _recorder = new ExtronSmp351(_mockClient.Object, 1024000, 512000);
+        _recorder.RecordStateHandlers += _recordStateHandler.Object;
+        _recorder.TimestampHandlers += _timestampHandler.Object;
+    }
+
+    [Theory]
+    [InlineData("<ChA1*ChB3>*<stopped>*<internal*auto>*<116606760*N/A>*<00:00:00>*<41:28:03*00:00:00>\r\n", RecordState.Stopped)]
+    [InlineData("<ChA1*ChB3>*<recording>*<internal*N/A>*<116606580*N/A>*<00:00:06>*<824:27:00*00:00:00>\r\n", RecordState.Recording)]
+    [InlineData("<ChA1*ChB3>*<paused>*<internal*N/A>*<116606376*N/A>*<00:00:13>*<824:26:40*00:00:00>\n\r\n", RecordState.RecordingPaused)]
+    public void HandleResponse_ReportsRecordingState(string response, RecordState expectedState)
+    {
+        _mockClient.Object.ResponseHandlers!.Invoke(response);
+        
+        _recordStateHandler.Verify(x => x.Invoke(expectedState));
+    }
+
+    [Theory]
+    [InlineData("<ChA1*ChB3>*<recording>*<internal*auto>*<116606760*N/A>*<00:00:06>*<41:28:03*00:00:00>\r\n", "00:00:06")]
+    [InlineData("<ChA1*ChB3>*<paused>*<internal*N/A>*<116606580*N/A>*<09:10:06>*<824:27:00*00:00:00>\r\n", "09:10:06")]
+    public void HandleResponse_ReportsRecordTime(string response, string expectedTime)
+    {
+        _mockClient.Object.ResponseHandlers!.Invoke(response);
+        
+        _timestampHandler.Verify(x => x.Invoke(expectedTime));
+    }
+    
+    [Fact]
+    public void HandleResponse_DoesNotReportRecordTimeWhenStopped()
+    {
+        var response = "<ChA1*ChB3>*<stopped>*<internal*auto>*<116606760*N/A>*<00:00:06>*<41:28:03*00:00:00>\r\n";
+        _mockClient.Object.ResponseHandlers!.Invoke(response);
+        
+        _timestampHandler.Verify(x => x.Invoke(It.IsAny<string>()), Times.Never);
+    }
+}
