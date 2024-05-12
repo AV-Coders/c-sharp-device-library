f4acaeb3b50c7dfd85d87e8ab05e3719c401a335
Anthony C <47967077+ant-car@users.noreply.github.com>
2024-05-13 09:51:28 +1000

    Add MotoluxBlindTransmitter

---
+++ Motor/MotoluxBlindTransmitter.cs
@@ -0,0 +1,73 @@
+﻿using AVCoders.Core;
+
+namespace AVCoders.Motor;
+
+public class MotoluxBlindTransmitter : Motor
+{
+    private readonly SerialClient _client;
+
+    private readonly byte _commandHeader = 0x9a;
+    private readonly byte _controlCommand = 0x0a;
+    private readonly byte _up = 0xDD;
+    private readonly byte _down = 0xEE;
+    private readonly byte _stop = 0xCC;
+
+    private readonly byte[] _lowerCommand;
+    private readonly byte[] _raiseCommand;
+    private readonly byte[] _stopCommand;
+
+    public MotoluxBlindTransmitter(string name, byte deviceId, byte blindId, RelayAction powerOnAction, int moveSeconds, SerialClient client)
+        : base(name, powerOnAction, moveSeconds)
+    {
+        _client = client;
+        (byte blindIdLow, byte blindIdHigh) = GetIdBytes(blindId);
+
+        byte lowerCommandChecksum = CalculateChecksum(new List<byte> { deviceId, blindIdLow, blindIdHigh, _controlCommand, _down });
+        _lowerCommand = new[] { _commandHeader, deviceId, blindIdLow, blindIdHigh, _controlCommand, _down, lowerCommandChecksum };
+        
+        byte raiseCommandChecksum = CalculateChecksum(new List<byte> { deviceId, blindIdLow, blindIdHigh, _controlCommand, _up });
+        _raiseCommand = new[] { _commandHeader, deviceId, blindIdLow, blindIdHigh, _controlCommand, _up, raiseCommandChecksum };
+        
+        byte stopCommandChecksum = CalculateChecksum(new List<byte> { deviceId, blindIdLow, blindIdHigh, _controlCommand, _stop });
+        _stopCommand = new[] { _commandHeader, deviceId, blindIdLow, blindIdHigh, _controlCommand, _stop, stopCommandChecksum };
+    }
+    
+    public (byte, byte) GetIdBytes(byte value)
+    {
+        byte highByte = 0;
+        byte lowByte = 0;
+
+        if (value < 8)
+        {
+            lowByte = (byte)(1 << value - 1);
+        }
+        else
+        {
+            highByte = (byte)(1 << (value - 9));
+        }
+
+        return (lowByte, highByte);
+    }
+
+    private byte CalculateChecksum(List<byte> command)
+    {
+        byte checksum = command[0];
+        command.ForEach(x => checksum ^= x);
+        return checksum;
+    }
+
+    public override void Raise()
+    {
+        _client.Send(_raiseCommand);
+    }
+
+    public override void Lower()
+    {
+        _client.Send(_lowerCommand);
+    }
+
+    public override void Stop()
+    {
+        _client.Send(_stopCommand);
+    }
+}
---
+++ MotorTest/MotoluxBlindTransmitterTest.cs
@@ -0,0 +1,61 @@
+﻿using AVCoders.Core;
+using Moq;
+
+namespace AVCoders.Motor.Tests;
+
+public class MotoluxBlindTransmitterTest
+{
+    private Mock<SerialClient> _mockClient;
+    private MotoluxBlindTransmitter _device;
+
+    public MotoluxBlindTransmitterTest()
+    {
+        _mockClient = new Mock<SerialClient>();
+        _device = new MotoluxBlindTransmitter("Blind", 0x00, 0x01, RelayAction.Lower, 30, _mockClient.Object);
+    }
+
+    [Fact]
+    public void Lower_SendsTheCommand()
+    {
+        _device.Lower();
+
+        byte[] expectedPayload = { 0x9a, 0x00, 0x01, 0x00, 0x0a, 0xEE, 0xE5 };
+        
+        _mockClient.Verify(x => x.Send(expectedPayload));
+    }
+
+    [Fact]
+    public void Raise_SendsTheCommand()
+    {
+        _device.Raise();
+
+        byte[] expectedPayload = { 0x9a, 0x00, 0x01, 0x00, 0x0a, 0xDD, 0xD6 };
+        
+        _mockClient.Verify(x => x.Send(expectedPayload));
+    }
+
+    [Fact]
+    public void Stop_SendsTheCommand()
+    {
+        _device.Stop();
+
+        byte[] expectedPayload = { 0x9a, 0x00, 0x01, 0x00, 0x0a, 0xCC, 0xC7 };
+        
+        _mockClient.Verify(x => x.Send(expectedPayload));
+    }
+
+    [Theory]
+    [InlineData(0, 0x00, 0x00)]
+    [InlineData(1, 0x01, 0x00)]
+    [InlineData(2, 0x02, 0x00)]
+    [InlineData(10, 0x00, 0x02)]
+    [InlineData(12, 0x00, 0x08)]
+    [InlineData(16, 0x00, 0x80)]
+    public void GetIdBytes_ReturnsTheCorrectValue(byte input, byte expectedLow, byte expectedHigh)
+    {
+        (byte actualLow, byte actualHigh) = _device.GetIdBytes(input);
+        
+        Assert.Equal(expectedLow, actualLow);
+        Assert.Equal(expectedHigh, actualHigh);
+    }
+}
