f450304f216a19fb670936fcefd9a93865041842
Anthony C <47967077+ant-car@users.noreply.github.com>
2024-06-03 12:45:44 +1000

    Add Bravia professional displays serial control

---
+++ Display/SonySerialControl.cs
@@ -0,0 +1,85 @@
+﻿using AVCoders.Core;
+
+namespace AVCoders.Display;
+
+public class SonySerialControl : Display
+{
+    public static readonly SerialBaud DefaultBaud = SerialBaud.Rate9600;
+    public static readonly SerialDataBits DefaultBits = SerialDataBits.DataBits8;
+    public static readonly SerialParity DefaultParity = SerialParity.None;
+    public static readonly SerialStopBits DefaultStopBits = SerialStopBits.Bits1;
+
+    private const char HeaderControl = '\u008C';
+    private const char HeaderQuery = '\u0083';
+    private const char HeaderAnswer = '\u0070';
+
+    private const char Category = '\u0000';
+    private const char FunctionPower = '\u0000';
+    private const char FunctionInput = '\u0002';
+    private const char FunctionVolume = '\u0005';
+    private const char FunctionMute = '\u0006';
+    private const char Length2IncludingChecksum = '\u0002';
+    private const char Length3IncludingChecksum = '\u0003';
+    private const char Direct = '\u0001';
+
+    // 8c+00+00+02 = 8e
+    private readonly char[] _powerOnCommand = { HeaderControl, Category, FunctionPower, Length2IncludingChecksum, '\u0001', '\u008f' };
+    private readonly char[] _powerOffCommand = { HeaderControl, Category, FunctionPower, Length2IncludingChecksum, '\u0000', '\u008e' };
+
+    // 8c + 00+05+03+1 = 95
+    private readonly char[] _volumeHeader = { HeaderControl, Category, FunctionVolume, Length3IncludingChecksum, Direct };
+    private readonly char _volumeHeaderChecksum = '\u0095';
+
+    private static readonly Dictionary<Input, char[]> InputDictionary = new ()
+    {
+        // 8c+00+02+03+04 = 95
+        { Input.Hdmi1, new []{ HeaderControl, Category, FunctionInput, Length3IncludingChecksum, '\u0004', '\u0001', '\u0096' }},
+        { Input.Hdmi2, new []{ HeaderControl, Category, FunctionInput, Length3IncludingChecksum, '\u0004', '\u0002', '\u0097' }},
+        { Input.Hdmi3, new []{ HeaderControl, Category, FunctionInput, Length3IncludingChecksum, '\u0004', '\u0003', '\u0098' }},
+        { Input.Hdmi4, new []{ HeaderControl, Category, FunctionInput, Length3IncludingChecksum, '\u0004', '\u0004', '\u0099' }},
+    };
+
+    private static readonly Dictionary<MuteState, char[]> MuteDictionary = new()
+    {
+        { MuteState.On , new [] { HeaderControl, Category, FunctionMute, Length3IncludingChecksum, Direct, '\u0001', '\u0097' }},
+        { MuteState.Off, new [] { HeaderControl, Category, FunctionMute, Length3IncludingChecksum, Direct, '\u0000', '\u0096' }}
+    };
+
+    private readonly SerialClient _client;
+
+    public SonySerialControl(SerialClient client) : base(InputDictionary.Keys.ToList())
+    {
+        _client = client;
+    }
+
+    protected override void Poll() => PollWorker.Stop();
+
+    private void SendCommand(char [] command) => _client.Send(command);
+
+    protected override void DoPowerOn() => SendCommand(_powerOnCommand);
+
+    protected override void DoPowerOff() => SendCommand(_powerOffCommand);
+
+    protected override void DoSetInput(Input input) => SendCommand(InputDictionary[input]);
+
+    protected override void DoSetAudioMute(MuteState state) => SendCommand(MuteDictionary[state]);
+
+    private byte CalculateChecksum(char[] command)
+    {
+        char checksum = '\u0000';
+        foreach (char value in command)
+        {
+            checksum += value;
+        }
+        return (byte)checksum;
+    }
+
+    protected override void DoSetVolume(int volume)
+    {
+        char volumeChar = (char)volume;
+        char checksum = (char)(_volumeHeaderChecksum + volumeChar);
+        var commandWithoutChecksum = _volumeHeader.Append(volumeChar);
+        var command = commandWithoutChecksum.Append(checksum);
+        SendCommand(command.ToArray());
+    }
+}
---
+++ DisplayTest/SonySerialControlTest.cs
@@ -0,0 +1,69 @@
+﻿using AVCoders.Core;
+using Moq;
+
+namespace AVCoders.Display.Tests;
+
+public class SonySerialControlTest
+{
+    private readonly SonySerialControl _display;
+    private readonly Mock<SerialClient> _mockClient;
+
+    public SonySerialControlTest()
+    {
+        _mockClient = new Mock<SerialClient>();
+        _display = new SonySerialControl(_mockClient.Object);
+    }
+    
+    [Fact]
+    public void PowerOff_SendsThePowerOffCommand()
+    {
+        char[] expectedPowerCommand = { '\u008C', '\u0000', '\u0000', '\u0002', '\u0000', '\u008e'  };
+        _display.PowerOff();
+
+        _mockClient.Verify(x => x.Send(expectedPowerCommand), Times.Once);
+    }
+    
+    
+
+    [Fact]
+    public void PowerOn_SendsThePowerOnCommand()
+    {
+        char[] expectedPowerOnCommand = { '\u008C', '\u0000', '\u0000', '\u0002','\u0001', '\u008f'  };
+        _display.PowerOn();
+
+        _mockClient.Verify(x => x.Send(expectedPowerOnCommand), Times.Once);
+    }
+    
+    [Theory]
+    [InlineData(Input.Hdmi1, new char[] { '\u008C','\u0000','\u0002','\u0003','\u0004','\u0001','\u0096' })]
+    [InlineData(Input.Hdmi2, new char[] { '\u008C','\u0000','\u0002','\u0003','\u0004','\u0002','\u0097' })]
+    [InlineData(Input.Hdmi3, new char[] { '\u008C','\u0000','\u0002','\u0003','\u0004','\u0003','\u0098' })]
+    [InlineData(Input.Hdmi4, new char[] { '\u008C','\u0000','\u0002','\u0003','\u0004','\u0004','\u0099' })]
+    public void SetInput_SendsTheExpectedCommand(Input source, char[] command)
+    {
+        _display.SetInput(source);
+
+        _mockClient.Verify(x => x.Send(command), Times.Once);
+    }
+    
+    [Theory]
+    [InlineData(0,   new char[] { '\u008C', '\u0000', '\u0005', '\u0003', '\u0001', '\u0000', '\u0095' })]
+    [InlineData(1,   new char[] { '\u008C', '\u0000', '\u0005', '\u0003', '\u0001', '\u0001', '\u0096' })]
+    [InlineData(100, new char[] { '\u008C', '\u0000', '\u0005', '\u0003', '\u0001', '\u0064', '\u00F9' })]
+    public void SetVolume_SendsTheExpectedCommand(int volume, char[] command)
+    {
+        _display.SetVolume(volume);
+
+        _mockClient.Verify(x => x.Send(command), Times.Once);
+    }
+    
+    [Theory]
+    [InlineData(MuteState.On,   new char[] { '\u008C','\u0000','\u0006','\u0003','\u0001','\u0001','\u0097'})]
+    [InlineData(MuteState.Off,   new char[] { '\u008C','\u0000','\u0006','\u0003','\u0001','\u0000','\u0096'})]
+    public void SetAudioMute_SendsTheExpectedCommand(MuteState state, char[] command)
+    {
+        _display.SetAudioMute(state);
+
+        _mockClient.Verify(x => x.Send(command), Times.Once);
+    }
+}
