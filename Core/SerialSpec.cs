837544dabf35aca4aec18a3aa6c2152861c73176
Anthony C <47967077+ant-car@users.noreply.github.com>
2024-06-17 12:30:01 +1000

    Clean up serialSpec

--- Core/SerialSpec.cs
+++ Core/SerialSpec.cs
@@ -1,24 +1,8 @@
 ﻿namespace AVCoders.Core;
 
-public class SerialSpec
-{
-    public SerialBaud BaudRate { get; }
-    public SerialParity Parity { get; }
-    public SerialDataBits DataBits { get; }
-    public SerialStopBits StopBits { get; }
-    public SerialProtocol Protocol { get; }
-
-    public SerialSpec(
-        SerialBaud baudRate,
-        SerialParity parity,
-        SerialDataBits dataBits,
-        SerialStopBits stopBits,
-        SerialProtocol protocol)
-    {
-        BaudRate = baudRate;
-        Parity = parity;
-        DataBits = dataBits;
-        StopBits = stopBits;
-        Protocol = protocol;
-    }
-}
+public record SerialSpec(
+    SerialBaud BaudRate,
+    SerialParity Parity,
+    SerialDataBits DataBits,
+    SerialStopBits StopBits,
+    SerialProtocol Protocol);
