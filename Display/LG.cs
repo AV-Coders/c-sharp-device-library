19c7ba5ed67f9f4ef46494e4caa55dd698759654
Anthony C <47967077+ant-car@users.noreply.github.com>
2024-07-18 19:16:19 +1000

    Add the LG protocol

---
+++ Display/LG.cs
@@ -0,0 +1,98 @@
+﻿using AVCoders.Core;
+using AVCoders.MediaPlayer;
+
+namespace AVCoders.Display;
+
+public class LG : Display, ISetTopBox
+{
+    // Source:
+    // https://www.lg.com/ca_en/support/product-support/troubleshoot/help-library/cs-CT52001643-20153058982994/
+    public static readonly ushort DefaultPort = 1717;
+    private CommunicationClient _comms;
+    private readonly int _setId;
+    private readonly string _pollArgument = "FF";
+    private readonly string _powerHeader = "ka";
+    private readonly string _inputHeader = "xb";
+    private readonly string _volumeHeader = "kf";
+    private readonly string _muteHeader = "ke";
+    private readonly string _irccHeader = "mc";
+    private readonly string _channelHeader = "ma";
+    private readonly Dictionary<Input, string> _inputDictionary = new()
+    {
+        { Input.Hdmi1, "90" },
+        { Input.Hdmi2, "91" },
+        { Input.Hdmi3, "92" },
+        { Input.Hdmi4, "93" },
+        { Input.DvbtTuner, "00" }
+    };
+    private readonly Dictionary<MuteState, string> _muteDictionary = new()
+    {
+        { MuteState.On, "00" },
+        { MuteState.Off, "01" }
+    };
+
+    private static readonly Dictionary<RemoteButton, string> RemoteButtonMap = new()
+    {
+        { RemoteButton.Button0, "10"},
+        { RemoteButton.Button1, "11"},
+        { RemoteButton.Button2, "12"},
+        { RemoteButton.Button3, "13"},
+        { RemoteButton.Button4, "14"},
+        { RemoteButton.Button5, "15"},
+        { RemoteButton.Button6, "16"},
+        { RemoteButton.Button7, "17"},
+        { RemoteButton.Button8, "18"},
+        { RemoteButton.Button9, "19"},
+        { RemoteButton.Enter, "44"},
+        { RemoteButton.Up, "40"},
+        { RemoteButton.Down, "41"},
+        { RemoteButton.Left, "07"},
+        { RemoteButton.Right, "06"},
+        { RemoteButton.Subtitle, "39"}
+    };
+
+    public LG(CommunicationClient comms, int setId) : base(new List<Input>
+    {
+        Input.Hdmi1, Input.Hdmi2, Input.Hdmi3, Input.Hdmi4, Input.DvbtTuner
+    })
+    {
+        _comms = comms;
+        _setId = setId;
+        
+        UpdateCommunicationState(CommunicationState.NotAttempted);
+    }
+
+    private void SendCommand(string header, string value) => _comms.Send($"{header} {_setId:d2} {value}\r");
+
+    protected override void Poll()
+    {
+        SendCommand(_powerHeader, _pollArgument);
+        if (PowerState == PowerState.On)
+        {
+            Thread.Sleep(1000);
+            SendCommand(_inputHeader, _pollArgument);
+            Thread.Sleep(1000);
+            SendCommand(_volumeHeader, _pollArgument);
+            Thread.Sleep(1000);
+            SendCommand(_muteHeader,  _pollArgument);
+        }
+    }
+
+    protected override void DoPowerOn() => SendCommand(_powerHeader, "01");
+
+    protected override void DoPowerOff() => SendCommand(_powerHeader, "00");
+
+    protected override void DoSetInput(Input input) => SendCommand(_inputHeader, _inputDictionary[input]);
+
+    protected override void DoSetVolume(int percentage) => SendCommand(_volumeHeader, $"{percentage:x2}");
+
+    protected override void DoSetAudioMute(MuteState state) => SendCommand(_muteHeader, _muteDictionary[state]);
+
+    public void ChannelUp() => SendCommand(_irccHeader, "00");
+
+    public void ChannelDown() => SendCommand(_irccHeader, "01");
+
+    public void SendIRCode(RemoteButton button) => SendCommand(_irccHeader, RemoteButtonMap[button]);
+
+    public void SetChannel(int channel) => SendCommand(_channelHeader, $"00 {channel:X2} 10");
+}
---
+++ DisplayTest/LgTest.cs
@@ -0,0 +1,80 @@
+﻿using AVCoders.Core;
+using Moq;
+
+namespace AVCoders.Display.Tests;
+
+public class LgTest
+{
+    private readonly LG _display;
+    private readonly Mock<TcpClient> _client;
+
+    public LgTest()
+    {
+        _client = new Mock<TcpClient>("foo", (ushort)1);
+        _display = new LG(_client.Object, 0);
+    }
+    
+    [Fact]
+    public void PowerOn_SendsThePowerOnCommand()
+    {
+        String expectedPowerOnCommand = "ka 00 01\r";
+        _display.PowerOn();
+
+        _client.Verify(x => x.Send(expectedPowerOnCommand), Times.Once);
+    }
+
+    [Fact]
+    public void PowerOff_SendsThePowerOffCommand()
+    {
+        String expectedPowerOffCommand = "ka 00 00\r";
+        _display.PowerOff();
+
+        _client.Verify(x => x.Send(expectedPowerOffCommand), Times.Once);
+    }
+    
+    [Theory]
+    [InlineData(Input.Hdmi1, "xb 00 90\r")]
+    [InlineData(Input.Hdmi2, "xb 00 91\r")]
+    [InlineData(Input.Hdmi3, "xb 00 92\r")]
+    [InlineData(Input.Hdmi4, "xb 00 93\r")]
+    [InlineData(Input.DvbtTuner, "xb 00 00\r")]
+    public void SetInput_SetsTheInput(Input input, string expectedInputCommand)
+    {
+        _display.SetInput(input);
+
+        _client.Verify(x => x.Send(expectedInputCommand), Times.Once);
+    }
+
+    [Theory]
+    [InlineData(0, "kf 00 00\r")]
+    [InlineData(50, "kf 00 32\r")]
+    [InlineData(100, "kf 00 64\r")]
+    public void SetVolume_SetsTheVolume(int volume, string expectedVolumeCommand)
+    {
+        _display.SetVolume(volume);
+
+        _client.Verify(x => x.Send(expectedVolumeCommand), Times.Once);
+    }
+    
+    [Theory]
+    [InlineData(MuteState.On, "ke 00 00\r")]
+    [InlineData(MuteState.Off, "ke 00 01\r")]
+    public void setAudioMute_SendsTheCommand(MuteState state, string expectedMuteCommand)
+    {
+        _display.SetAudioMute(state);
+
+        _client.Verify(x => x.Send(expectedMuteCommand), Times.Once);
+    }
+
+    [Theory]
+    [InlineData(1, "ma 00 00 01 10\r")]
+    [InlineData(10, "ma 00 00 0A 10\r")]
+    [InlineData(72, "ma 00 00 48 10\r")]
+    [InlineData(99, "ma 00 00 63 10\r")]
+    public void SetChannel_SendsTheCommand(int channel, string expectedCommand)
+    {
+        _display.SetChannel(channel);
+        
+        _client.Verify(x => x.Send(expectedCommand), Times.Once);
+    }
+}
