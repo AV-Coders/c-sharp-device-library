899ce249675a93f9f843472be0ee77722c3a2565
Anthony C <47967077+ant-car@users.noreply.github.com>
2025-01-14 10:31:27 +1100

    Add Philips SICP

---
+++ Display/PhilipsSICP.cs
@@ -0,0 +1,85 @@
+using AVCoders.Core;
+
+namespace AVCoders.Display;
+
+public class PhilipsSICP : Display
+{
+    public const ushort DefaultPort = 5000;
+    public static readonly SerialSpec DefaultSpec = new (SerialBaud.Rate9600, SerialParity.None,
+        SerialDataBits.DataBits8, SerialStopBits.Bits1, SerialProtocol.Rs232); 
+
+    private readonly byte _monitorId;
+    private readonly byte _group0 = 0x00;
+    private CommunicationClient _client;
+
+    private static readonly Dictionary<Input, byte> _inputMap = new Dictionary<Input, byte>
+    {
+        { Input.Hdmi1, 0x0d},
+        { Input.Hdmi2, 0x06},
+        { Input.Hdmi3, 0x0F},
+        { Input.Hdmi4, 0x19},
+    };
+
+    public PhilipsSICP(CommunicationClient client, byte monitorId, string name, Input? defaultInput, int pollTime = 23) : base(
+        _inputMap.Keys.ToList(), name, defaultInput, pollTime)
+    {
+        _client = client;
+        _monitorId = monitorId;
+    }
+
+
+    protected override Task Poll(CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Send(byte[] data)
+    {
+        byte messageSize = (byte)(data.Length + 4);
+        byte checksum = GenerateChecksum(messageSize, data);
+        byte[] payload = new byte[messageSize];
+        
+        payload[0] = messageSize;
+        payload[1] = _monitorId;
+        payload[2] = _group0;
+        Array.Copy(data, 0, payload, 3, data.Length);
+        payload[messageSize - 1] = checksum;
+        _client.Send(payload);
+    }
+
+    private byte GenerateChecksum(byte messageSize, byte[] bytes)
+    {
+        byte checksum = messageSize;
+        foreach (var t in bytes)
+        {
+            checksum ^= t;
+        }
+
+        return checksum;
+    }
+
+    protected override void DoPowerOn()
+    {
+        Send(new byte[] { 0x18, 0x02 });
+    }
+
+    protected override void DoPowerOff()
+    {
+        Send(new byte[] { 0x18, 0x01 });
+    }
+
+    protected override void DoSetInput(Input input)
+    {
+        Send(new byte[] { 0xAC, _inputMap[input], 0x00, 0x01, 0x00 });
+    }
+
+    protected override void DoSetVolume(int percentage)
+    {
+        Send(new byte[] { 0x44, (byte)percentage, (byte)percentage });
+    }
+
+    protected override void DoSetAudioMute(MuteState state)
+    {
+        Send(new byte[] { 0x47, (byte)(state == MuteState.On ? 0x01 : 0x00) });
+    }
+}
---
+++ DisplayTest/PhilipsSICPTest.cs
@@ -0,0 +1,94 @@
+using AVCoders.Core;
+using Moq;
+
+namespace AVCoders.Display.Tests;
+
+public class PhilipsSICPTest
+{
+    private readonly Mock<TcpClient> _mockClient;
+    private readonly PhilipsSICP _display;
+    private readonly byte _displayId = 0x00;
+
+    public PhilipsSICPTest()
+    {
+        _mockClient = new("foo", PhilipsSICP.DefaultPort, "Test");
+        _display = new PhilipsSICP(_mockClient.Object, _displayId, "Test", Input.Hdmi1);
+    }
+    
+    [Fact]
+    public void PowerOn_SendsThePowerOnCommand()
+    {
+        byte[] expectedPowerOnCommand = { 0x06, 0x00, 0x00, 0x18, 0x02, 0x1C };
+        _display.PowerOn();
+
+        _mockClient.Verify(x => x.Send(expectedPowerOnCommand), Times.Once);
+    }
+
+    [Fact]
+    public void PowerOn_UsesTheCorrectDisplayId()
+    {
+        PhilipsSICP display2 = new PhilipsSICP(_mockClient.Object, 0x05, "Test display", Input.Hdmi1);
+        byte[] expectedPowerOnCommand = { 0x06, 0x05, 0x00, 0x18, 0x02, 0x1C };
+
+        display2.PowerOn();
+        _mockClient.Verify(x => x.Send(expectedPowerOnCommand), Times.Once);
+    }
+
+    [Fact]
+    public void PowerOff_SendsThePowerOffCommand()
+    {
+        byte[] expectedPowerOnCommand = { 0x06, 0x00, 0x00, 0x18, 0x01, 0x1F };
+        _display.PowerOff();
+
+        _mockClient.Verify(x => x.Send(expectedPowerOnCommand), Times.Once);
+    }
+
+    [Fact]
+    public void PowerOff_UsesTheCorrectDisplayId()
+    {
+        PhilipsSICP display2 = new PhilipsSICP(_mockClient.Object, 0x04, "Test display", Input.Hdmi1);
+        byte[] expectedPowerOnCommand = { 0x06, 0x04, 0x00, 0x18, 0x01, 0x1F };
+
+        display2.PowerOff();
+        _mockClient.Verify(x => x.Send(expectedPowerOnCommand), Times.Once);
+    }
+
+    [Theory]
+    [InlineData(Input.Hdmi1, new byte[] { 0x09, 0x00, 0x00, 0xAC, 0x0D, 0x00, 0x01, 0x00, 0xA9 })]
+    [InlineData(Input.Hdmi2, new byte[] { 0x09, 0x00, 0x00, 0xAC, 0x06, 0x00, 0x01, 0x00, 0xA2 })]
+    [InlineData(Input.Hdmi3, new byte[] { 0x09, 0x00, 0x00, 0xAC, 0x0F, 0x00, 0x01, 0x00, 0xAB })]
+    [InlineData(Input.Hdmi4, new byte[] { 0x09, 0x00, 0x00, 0xAC, 0x19, 0x00, 0x01, 0x00, 0xBD })]
+    public void SetInput_SendsTheCommand(Input input, byte[] expectedCommand)
+    {
+        _display.SetInput(input);
+        
+        _mockClient.Verify(x => x.Send(expectedCommand), Times.Once);
+    }
+
+    [Theory]
+    [InlineData(MuteState.On, new byte[] { 0x06, 0x00, 0x00, 0x47, 0x01, 0x40 })]
+    [InlineData(MuteState.Off, new byte[] { 0x06, 0x00, 0x00, 0x47, 0x00, 0x41 })]
+    public void SetAudioMute_SendsTheCommand(MuteState state, byte[] expectedState)
+    {
+        _display.SetAudioMute(state);
+        _mockClient.Verify(x => x.Send(expectedState), Times.Once);
+    }
+
+    [Theory]
+    [InlineData(0, new byte[] { 0x07, 0x00, 0x00, 0x44, 0x00, 0x00, 0x43 })]
+    [InlineData(10, new byte[] { 0x07, 0x00, 0x00, 0x44, 0x0A, 0x0A, 0x43 })]
+    [InlineData(20, new byte[] { 0x07, 0x00, 0x00, 0x44, 0x14, 0x14, 0x43 })]
+    [InlineData(30, new byte[] { 0x07, 0x00, 0x00, 0x44, 0x1E, 0x1E, 0x43 })]
+    [InlineData(40, new byte[] { 0x07, 0x00, 0x00, 0x44, 0x28, 0x28, 0x43 })]
+    [InlineData(50, new byte[] { 0x07, 0x00, 0x00, 0x44, 0x32, 0x32, 0x43 })]
+    [InlineData(60, new byte[] { 0x07, 0x00, 0x00, 0x44, 0x3C, 0x3C, 0x43 })]
+    [InlineData(70, new byte[] { 0x07, 0x00, 0x00, 0x44, 0x46, 0x46, 0x43 })]
+    [InlineData(80, new byte[] { 0x07, 0x00, 0x00, 0x44, 0x50, 0x50, 0x43 })]
+    [InlineData(90, new byte[] { 0x07, 0x00, 0x00, 0x44, 0x5A, 0x5A, 0x43 })]
+    [InlineData(100, new byte[] { 0x07, 0x00, 0x00, 0x44, 0x64, 0x64, 0x43 })]
+    public void SetVolume_SendsTheCommand(int volume, byte[] expectedCommand)
+    {
+        _display.SetVolume(volume);
+        _mockClient.Verify(x => x.Send(expectedCommand), Times.Once);
+    }
+}
