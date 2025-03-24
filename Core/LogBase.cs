e0e72fc376fcae781f4d26c28f312083735bb5a5
Anthony C <47967077+ant-car@users.noreply.github.com>
2025-03-24 15:24:21 +1100

    Create LogBase.cs

---
+++ Core/LogBase.cs
@@ -0,0 +1,70 @@
+﻿using Serilog;
+using Serilog.Context;
+
+namespace AVCoders.Core;
+
+public abstract class LogBase
+{
+    protected readonly string Name;
+
+    protected LogBase(string name)
+    {
+        Name = name;
+    }
+
+    protected void Verbose(string message)
+    {
+        using (LogContext.PushProperty("class", GetType()))
+        using (LogContext.PushProperty("instance_name", Name))
+        {
+            Log.Verbose(message);
+        }
+    }
+    
+    protected void Debug(string message)
+    {
+        using (LogContext.PushProperty("class", GetType()))
+        using (LogContext.PushProperty("instance_name", Name))
+        {
+            Log.Debug(message);
+        }
+    }
+    
+    protected void Info(string message)
+    {
+        using (LogContext.PushProperty("class", GetType()))
+        using (LogContext.PushProperty("instance_name", Name))
+        {
+            Log.Information(message);
+        }
+    }
+    
+    protected void Warn(string message)
+    {
+        using (LogContext.PushProperty("class", GetType()))
+        using (LogContext.PushProperty("instance_name", Name))
+        {
+            Log.Warning(message);
+        }
+    }
+
+    protected void Error(string message)
+    {
+        
+        using (LogContext.PushProperty("class", GetType()))
+        using (LogContext.PushProperty("instance_name", Name))
+        {
+            Log.Error(message);
+        }
+    }
+
+    protected void Fatal(string message)
+    {
+        
+        using (LogContext.PushProperty("class", GetType()))
+        using (LogContext.PushProperty("instance_name", Name))
+        {
+            Log.Fatal(message);
+        }
+    }
+}
