264dd896a47062f7bd0a312382c4868afc08efae
Anthony C <47967077+ant-car@users.noreply.github.com>
2025-07-08 11:06:00 +1000

    Add Annotator

---
+++ Annotator/ExtronAnnotator401.cs
@@ -0,0 +1,80 @@
+﻿using AVCoders.Core;
+using Serilog;
+
+namespace Annotator;
+
+public class ExtronAnnotator401 : DeviceBase
+{
+    public static readonly ushort DefaultPort = 22023;
+    private readonly CommunicationClient _client;
+    private readonly ThreadWorker _pollWorker;
+    private const string EscapeHeader = "\x1b";
+
+    public ExtronAnnotator401(CommunicationClient client, string name) : base(name)
+    {
+        _client = client;
+        _pollWorker = new ThreadWorker(Poll, TimeSpan.FromSeconds(20), true);
+        _pollWorker.Restart();
+        _client.ConnectionStateHandlers += HandleConnectionState;
+        _client.ResponseHandlers += HandleResponse;
+    }
+
+    private void HandleResponse(string value)
+    {
+        
+    }
+
+    public void Clear() => WrapAndSendCommand("0EDIT");
+
+    public void StartCalibration() => WrapAndSendCommand("1PCAL");
+
+    public void StopCalibration() => WrapAndSendCommand("0PCAL");
+
+    private void HandleConnectionState(ConnectionState connectionState)
+    {
+        if(connectionState != ConnectionState.Connected)
+            return;
+        WrapAndSendCommand("3CV");
+        Thread.Sleep(TimeSpan.FromSeconds(1));
+        WrapAndSendCommand("ASHW");
+        Thread.Sleep(TimeSpan.FromSeconds(1));
+    }
+
+    private Task Poll(CancellationToken arg)
+    {
+        if(_client.ConnectionState != ConnectionState.Connected)
+            return Task.CompletedTask;
+        
+        WrapAndSendCommand("0TC");
+        return Task.CompletedTask;
+    }
+
+    public override void PowerOn() { }
+
+    public override void PowerOff() { }
+    
+    private void WrapAndSendCommand(string command) => SendCommand($"{EscapeHeader}{command}\r");
+
+    private void SendCommand(String command)
+    {
+        try
+        {
+            Log.Information("Sending {command}", command);
+            _client.Send(command);
+            CommunicationState = CommunicationState.Okay;
+        }
+        catch (Exception e)
+        {
+            Error(e.Message);
+            CommunicationState = CommunicationState.Error;
+        }
+    }
+
+    public void SaveToNetworkDrive() => WrapAndSendCommand("3MCAP");
+
+    public void SaveToInternalMemory() => WrapAndSendCommand("0MCAP");
+
+    public void SaveToIQC() => WrapAndSendCommand("1MCAP");
+
+    public void SaveToUSB() => WrapAndSendCommand("2MCAP");
+}
---
+++ AnnotatorTest/ExtronAnnotator401Test.cs
@@ -0,0 +1,66 @@
+using Annotator;
+using AVCoders.Core;
+using Moq;
+
+namespace AnnotatorTest;
+
+public class ExtronAnnotator401Test
+{
+    private readonly ExtronAnnotator401 _annotator;
+    private readonly Mock<CommunicationClient> _mockClient = new ("foo");
+    private const string EscapeHeader = "\x1b";
+
+    public ExtronAnnotator401Test()
+    {
+        _annotator = new ExtronAnnotator401(_mockClient.Object, "Test annotator");
+    }
+    
+    [Fact]
+    public void Clear_SendsTheCommand()
+    {
+        _annotator.Clear();
+        _mockClient.Verify(x => x.Send($"{EscapeHeader}0EDIT\r"));
+    }
+
+    [Fact]
+    public void StartCalibration_SendsTheCommand()
+    {
+        _annotator.StartCalibration();
+        _mockClient.Verify(x => x.Send($"{EscapeHeader}1PCAL\r"));
+    }
+
+    [Fact]
+    public void StopCalibration_SendsTheCommand()
+    {
+        _annotator.StopCalibration();
+        _mockClient.Verify(x => x.Send($"{EscapeHeader}0PCAL\r"));
+    }
+
+    [Fact]
+    public void SaveToNetworkDrive_SendsTheCommand()
+    {
+        _annotator.SaveToNetworkDrive();
+        _mockClient.Verify(x => x.Send($"{EscapeHeader}3MCAP\r"));
+    }
+
+    [Fact]
+    public void SaveToInternalMemory_SendsTheCommand()
+    {
+        _annotator.SaveToInternalMemory();
+        _mockClient.Verify(x => x.Send($"{EscapeHeader}0MCAP\r"));
+    }
+
+    [Fact]
+    public void SaveToIQC_SendsTheCommand()
+    {
+        _annotator.SaveToIQC();
+        _mockClient.Verify(x => x.Send($"{EscapeHeader}1MCAP\r"));
+    }
+
+    [Fact]
+    public void SaveToUSB_SendsTheCommand()
+    {
+        _annotator.SaveToUSB();
+        _mockClient.Verify(x => x.Send($"{EscapeHeader}2MCAP\r"));
+    }
+}
