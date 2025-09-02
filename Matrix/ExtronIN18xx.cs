105bae116f363c1311373e882ef439c39b00786e
Anthony C <47967077+ant-car@users.noreply.github.com>
2025-09-02 16:37:01 +1000

    Add Extron IN18xx

---
+++ Matrix/ExtronIN18xx.cs
@@ -0,0 +1,61 @@
+﻿using AVCoders.Core;
+
+namespace AVCoders.Matrix;
+
+public class ExtronIn18Xx : VideoMatrix
+{
+    private readonly int _numberOfInputs;
+    public static readonly SerialSpec DefaultSerialSpec =
+        new (SerialBaud.Rate9600, SerialParity.None, SerialDataBits.DataBits8, SerialStopBits.Bits1, SerialProtocol.Rs232);
+
+    public ExtronIn18Xx(CommunicationClient communicationClient, int numberOfInputs, string name) : base(1, communicationClient, name)
+    {
+        _numberOfInputs = numberOfInputs;
+        PowerState = PowerState.Unknown;
+        UpdateCommunicationState(CommunicationState.NotAttempted);
+    }
+
+    private void SendCommand(string command)
+    {
+        try
+        {
+            CommunicationClient.Send(command);
+            UpdateCommunicationState(CommunicationState.Okay);
+        }
+        catch (Exception e)
+        {
+            LogException(e);
+            UpdateCommunicationState(CommunicationState.Error);
+        }
+    }
+
+    public override void RouteAV(int input, int output)
+    {
+        if (input > 0 && input <= _numberOfInputs)
+            SendCommand($"{input}*1!");
+    }
+
+    public override void PowerOn() {    }
+
+    public override void PowerOff() {    }
+
+    public override void RouteVideo(int input, int output)
+    {
+        if (input > 0 && input <= _numberOfInputs)
+            SendCommand($"{input}*1%");
+    }
+
+    public override void RouteAudio(int input, int output)
+    {
+        if (input > 0 && input <= _numberOfInputs)
+            SendCommand($"{input}*1$");
+    }
+
+    public void SetSyncTimeout(int seconds)
+    {
+        if (seconds < 502)
+        {
+            SendCommand($"\u001bT{seconds}SSAV\u0027");
+        }
+    }
+}
---
+++ MatrixTest/ExtronIn18xxTest.cs
@@ -0,0 +1,178 @@
+using System.Reflection;
+using AVCoders.Core;
+using Moq;
+
+namespace AVCoders.Matrix.Tests;
+
+public class ExtronIn18xxTest
+{
+    private readonly ExtronIn18Xx _switcher;
+    private readonly Mock<CommunicationClient> _mockClient;
+
+    public ExtronIn18xxTest()
+    {
+        _mockClient = new Mock<CommunicationClient>("foo");
+        _switcher = new ExtronIn18Xx(_mockClient.Object, 6, "test matrix");
+    }
+
+    [Fact]
+    public void SendCommand_DoesNotManipulateInput()
+    {
+        string input = "Foo";
+
+        var method = _switcher.GetType().GetMethod("SendCommand", BindingFlags.Instance | BindingFlags.NonPublic);
+        method?.Invoke(_switcher, [input]);
+        _mockClient.Verify(x => x.Send(input), Times.Once);
+    }
+
+    [Fact]
+    public void SendCommand_ReportsCommunicationIsOkay()
+    {
+        string input = "Foo";
+
+        var method = _switcher.GetType().GetMethod("SendCommand", BindingFlags.Instance | BindingFlags.NonPublic);
+        method?.Invoke(_switcher, [input]);
+
+        Assert.Equal(CommunicationState.Okay, _switcher.CommunicationState);
+    }
+
+    [Fact]
+    public void SendCommand_ReportsCommunicationHasFailed()
+    {
+        string input = "Foo";
+        _mockClient.Setup(client => client.Send(It.IsAny<string>())).Throws(new IOException("Oh No!"));
+
+        var method = _switcher.GetType().GetMethod("SendCommand", BindingFlags.Instance | BindingFlags.NonPublic);
+        method?.Invoke(_switcher, [input]);
+
+        Assert.Equal(CommunicationState.Error, _switcher.CommunicationState);
+    }
+
+    [Theory]
+    [InlineData(1, "1*1%")]
+    [InlineData(2, "2*1%")]
+    [InlineData(3, "3*1%")]
+    [InlineData(4, "4*1%")]
+    [InlineData(5, "5*1%")]
+    [InlineData(6, "6*1%")]
+    public void RouteVideo_SendsTheCommand(int input, string expectedRouteCommand)
+    {
+        _switcher.RouteVideo(input, 0);
+
+        _mockClient.Verify(x => x.Send(expectedRouteCommand), Times.Once);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(7)] // Constructed as an in1606
+    [InlineData(9)]
+    public void RouteVideo_IgnoresInvalidInputNumbers(int input)
+    {
+        _switcher.RouteVideo(input, 3);
+
+        _mockClient.Verify(x => x.Send(It.IsAny<string>()), Times.Never);
+    }
+
+    [Fact]
+    public void RouteVideo_IgnoresOutputParameter()
+    {
+        string expectedRouteCommand = "3*1%";
+        _switcher.RouteVideo(3, 6);
+
+        _mockClient.Verify(x => x.Send(expectedRouteCommand), Times.Once);
+    }
+
+    [Theory]
+    [InlineData(1, "1*1$")]
+    [InlineData(2, "2*1$")]
+    [InlineData(3, "3*1$")]
+    [InlineData(4, "4*1$")]
+    [InlineData(5, "5*1$")]
+    [InlineData(6, "6*1$")]
+    public void RouteAudio_SendsTheCommand(int input, string expectedRouteCommand)
+    {
+        _switcher.RouteAudio(input, 0);
+
+        _mockClient.Verify(x => x.Send(expectedRouteCommand), Times.Once);
+    }
+
+    [Fact]
+    public void RouteAudio_IgnoresOutputParameter()
+    {
+        string expectedRouteCommand = "1*1$";
+        _switcher.RouteAudio(1, 10);
+
+        _mockClient.Verify(x => x.Send(expectedRouteCommand), Times.Once);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(7)] // Constructed as an in1606
+    [InlineData(9)]
+    public void RouteAudio_IgnoresInvalidInputNumbers(int input)
+    {
+        _switcher.RouteAudio(input, 3);
+
+        _mockClient.Verify(x => x.Send(It.IsAny<string>()), Times.Never);
+    }
+
+    [Theory]
+    [InlineData(1, "1*1!")]
+    [InlineData(2, "2*1!")]
+    [InlineData(3, "3*1!")]
+    [InlineData(4, "4*1!")]
+    [InlineData(5, "5*1!")]
+    [InlineData(6, "6*1!")]
+    public void RouteAV_SendsTheCommand(int input, string expectedRouteCommand)
+    {
+        _switcher.RouteAV(input, 3);
+
+        _mockClient.Verify(x => x.Send(expectedRouteCommand), Times.Once);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(7)] // Constructed as an in1606
+    [InlineData(9)]
+    public void RouteAV_IgnoresInvalidInputNumbers(int input)
+    {
+        _switcher.RouteAV(input, 3);
+
+        _mockClient.Verify(x => x.Send(It.IsAny<string>()), Times.Never);
+    }
+
+    [Fact]
+    public void RouteAV_IgnoresOutputParameter()
+    {
+        string expectedRouteCommand = "2*1!";
+        _switcher.RouteAV(2, 3);
+
+        _mockClient.Verify(x => x.Send(expectedRouteCommand), Times.Once);
+    }
+
+    [Fact]
+    public void SetSyncTimeout_SendsTheCommand()
+    {
+        string expectedCommand = "\u001bT0SSAV\u0027";
+        _switcher.SetSyncTimeout(0);
+
+        _mockClient.Verify(x => x.Send(expectedCommand), Times.Once);
+    }
+
+    [Fact]
+    public void SetSyncTimeout_IgnoresInvalidTimeouts()
+    {
+        _switcher.SetSyncTimeout(502);
+
+        _mockClient.Verify(x => x.Send(It.IsAny<string>()), Times.Never);
+    }
+
+    [Fact]
+    public void SetSyncTimeout_SetsToNeverDropSync()
+    {
+        string expectedCommand = "\u001bT501SSAV\u0027";
+        _switcher.SetSyncTimeout(501);
+
+        _mockClient.Verify(x => x.Send(expectedCommand), Times.Once);
+    }
+}
