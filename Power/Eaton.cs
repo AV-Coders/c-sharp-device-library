3abe35428737abdb8dd004d15d1733d3a3c65759
Anthony C <47967077+ant-car@users.noreply.github.com>
2025-10-09 09:38:20 +1100

    Add Eaton PDU and outlets
    It's currently hard-coded to my current use case due to issues walking the table on the PDU.  This will be fixed in a future release.

---
+++ Power/Eaton.cs
@@ -0,0 +1,123 @@
+﻿using System.Net;
+using AVCoders.CommunicationClients;
+using AVCoders.Core;
+using Lextm.SharpSnmpLib;
+using Lextm.SharpSnmpLib.Messaging;
+using Lextm.SharpSnmpLib.Security;
+using Serilog;
+
+namespace AVCoders.Power;
+
+public class EatonOutlet : Outlet
+{
+    public readonly int OutletNumber;
+    public readonly int DeviceIndex;
+    private readonly EatonPdu _pdu;
+    private readonly ThreadWorker _pollWorker;
+    
+    public EatonOutlet(string name, EatonPdu pdu, int outletNumber, int deviceIndex) : base(name)
+    {
+        _pdu = pdu;
+        OutletNumber = outletNumber;
+        DeviceIndex = deviceIndex;
+        _pollWorker = new ThreadWorker(PollPowerState, TimeSpan.FromSeconds(12));
+        _pollWorker.Restart();
+    }
+
+    private Task PollPowerState(CancellationToken arg)
+    {
+        PowerState = _pdu.GetPowerState(this);
+        return Task.CompletedTask;   
+    }
+
+    public override void PowerOn()
+    {
+        _pdu.PowerOn(this);
+        Thread.Sleep(1000);
+        PowerState = _pdu.GetPowerState(this);
+    }
+
+    public override void PowerOff()
+    {
+        _pdu.PowerOff(this);
+        Thread.Sleep(1000);
+        PowerState = _pdu.GetPowerState(this);
+    }
+
+    public override void Reboot()
+    {
+        _pdu.Cycle(this);
+        Thread.Sleep(1000);
+        PowerState = _pdu.GetPowerState(this);
+        Thread.Sleep(5000);
+        PowerState = _pdu.GetPowerState(this);
+    }
+}
+
+public class EatonPdu : Pdu
+{
+    private readonly AvCodersSnmpV3Client _client;
+    private ThreadWorker _waitForConnectionWorker;
+
+    public EatonPdu(string name, AvCodersSnmpV3Client client) : base(name, client)
+    {
+        _client = client;
+        _waitForConnectionWorker = new ThreadWorker(Initialise, TimeSpan.FromSeconds(12));
+    }
+
+    private Task Initialise(CancellationToken arg)
+    {
+        var agent = _client.Get(".1.3.6.1.4.1.850.1.2.1.1.1.0");
+        if (agent.Count == 0)
+        {
+            Log.Error("Could not get agent");
+            return Task.CompletedTask;
+        }
+
+        Outlets.Clear();
+        for (int i = 1; i < 9; i++)
+        {
+            string name = _client.Get($".1.3.6.1.4.1.850.1.1.3.4.3.3.1.1.2.1.{i}")[0].Data.ToString();
+            Outlets.Add(new EatonOutlet(name, this, i, 1));
+        }
+        _waitForConnectionWorker.Stop();
+        OutletDefinitionHandlers?.Invoke(Outlets);
+        return Task.CompletedTask;
+    }
+
+    public override void PowerOn()
+    {
+        Outlets.ForEach(x => PowerOn(x as EatonOutlet));
+    }
+
+    public override void PowerOff()
+    {
+        Outlets.ForEach(x => PowerOff(x as EatonOutlet));
+    }
+
+    public void PowerOn(EatonOutlet outlet)
+    {
+        _client.Set($".1.3.6.1.4.1.850.1.1.3.2.3.3.1.1.6.{outlet.DeviceIndex}.{outlet.OutletNumber}", "2");
+    }
+
+    public void PowerOff(EatonOutlet outlet)
+    {
+        _client.Set($".1.3.6.1.4.1.850.1.1.3.2.3.3.1.1.6.{outlet.DeviceIndex}.{outlet.OutletNumber}", "1");
+    }
+
+    public void Cycle(EatonOutlet outlet)
+    {
+        _client.Set($".1.3.6.1.4.1.850.1.1.3.2.3.3.1.1.6.{outlet.DeviceIndex}.{outlet.OutletNumber}", "3");
+    }
+    
+    public PowerState GetPowerState(EatonOutlet outlet)
+    {
+        var response = _client.Get($".1.3.6.1.4.1.850.1.1.3.2.3.3.1.1.4.{outlet.DeviceIndex}.{outlet.OutletNumber}");
+        return response[0].Data.ToString() switch
+        {
+            "1" => PowerState.Off,
+            "2" => PowerState.On,
+            _ => PowerState.Unknown
+        }; 
+    }
+}
